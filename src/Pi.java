c5e3493b353d23417508e518fcdeac15d7168e30
ezevallos <ezevalloslabarthe@gmail.com>
2020-06-08 19:55:17 -0500

    Programa base

--- src/Pi.java
+++ src/Pi.java
@@ -1,5 +1,16 @@
+package pi;
+//import java.util.concurrent.locks.*;
+
 public class Pi {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        long t_inicial = System.currentTimeMillis();
+        double val = PiVal.getPi();
+        long t_final = System.currentTimeMillis();
+        System.out.println("Valor aproximado: " + val);
+        System.out.println("Valor verdadero de pi:" + Math.PI);
+        System.out.println("Diferencia:" + Math.abs(Math.PI - val));
+        System.out.println("Duración del programa:" + (t_final - t_inicial) + " milisegundos.");
     }
 }
