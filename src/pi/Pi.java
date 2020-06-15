0ff25a69ec77e4093aeea0221c7a041171171dfa
ezevallos <ezevalloslabarthe@gmail.com>
2020-06-14 19:39:52 -0500

    Debugging and cleanup

--- src/pi/Pi.java
+++ src/pi/Pi.java
@@ -1,31 +1,27 @@
 package pi;
 import java.io.FileWriter;
-//import java.util.concurrent.locks.*;
 import java.math.BigDecimal;
 
 public class Pi {
     public static void main(String[] args) throws Exception {
         int nHilos = Integer.parseInt(System.console().readLine("Determine cantidad de threads:"));
         int nDigitos = Integer.parseInt(System.console().readLine("Determine cantidad de digitos:"));
-        //MonteCarlo ValorPi = new MonteCarlo(nLanzamientos, nHilos);
-
-        //JohnMachin ValorPi = new JohnMachin(0,0);
+        
         JMMult valorPi = new JMMult();
 
         long t_inicial = System.currentTimeMillis();
         BigDecimal pi = valorPi.calculaPi(nHilos, nDigitos);
         long t_final = System.currentTimeMillis();
-        printTextFile(pi);
-        System.out.println("Valor verdadero de pi:" + Math.PI);
-        //System.out.println("Diferencia:" + Math.abs(Math.PI - val));
+        printTextFile(pi, (t_final - t_inicial));
         System.out.println("Duración del programa:" + (t_final - t_inicial) + " milisegundos.");
     }
 
-    public static void printTextFile(BigDecimal valor){
+    public static void printTextFile(BigDecimal valor, long tiempo){
         String filename = "valor_pi_"+(System.currentTimeMillis()/1000)+".txt";
         try {
             FileWriter writer = new FileWriter(filename);
-            writer.write(valor.toString());
+            writer.write("Tiempo: " + tiempo + "milisegundos.\n");
+            writer.append(valor.toString());
             writer.close();
             System.out.println("Pi impreso en: "+filename);
         } catch (Exception e) {
